6b7c928b6acbfeb1d47a31a041675e90f6f731c2
kaczormp4 <kaczkaoryginal@gmail.com>
2022-11-05 17:10:06 +0100

    4

--- JS/4. arrays.js
+++ JS/4. arrays.js
@@ -1,10 +1,24 @@
-// Utwórz tablicę o nazwie tab1, zawierającą następujące liczby całkowite: 7, 3, 1, 6, 9, 5, 4, 10, 2, 2.
+// ARRAYS
 
-// 1. Wypisz zawartość 5 komórki tablicy na ekran. Zmień zawartość 7 komórki tablicy, wprowadzając do niej wartość 12.
-// 2. wypisz długosc tablicy
-// 3. wypisz sume pierwszych trzech elementow tablicy
-// 4. wypisz ostatni element tablicy
-// 5. dopisz liczbe 232 do tablicy (uzyj metody push)
-// 6. usun ostatni element tablicy
-// 7. utworz zmienna  age = 22 i wpisz ją na poczatek tablicy
-// 8* połącz tablice tab1 z tablica [32,32,32,32]
+// ***************
+// *** ZADANIA ***
+// ***************
+
+// 1)
+// a) Omow czym jest tablica (array)
+// b) Utworz pustą tablice i przypisz ją do zmiennej arr.
+
+// 2)
+// Utwórz tablicę o nazwie tab1, zawierającą następujące liczby całkowite: 7, 3, 1, 6, 9, 5, 4, 10, 2, 2.
+// a) Wypisz zawartość 5 komórki tablicy na ekran. Zmień zawartość 7 komórki tablicy, wprowadzając do niej wartość 12.
+// b) wypisz długosc tablicy
+// c) wypisz sume pierwszych trzech elementow tablicy
+// d) wypisz ostatni element tablicy
+// e) dopisz liczbe 232 do tablicy (uzyj metody push)
+// f) usun ostatni element tablicy
+// g) utworz zmienna  age = 22 i wpisz ją na poczatek tablicy
+// h) połącz tablice tab1 z tablica [32,32,32,32]
+
+// ******************
+// *** POPRAW KOD ***
+// ******************
