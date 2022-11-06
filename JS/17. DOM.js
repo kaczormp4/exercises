401b51c0965ce70b0ad842f2283a446058ae8ded
kaczormp4 <kaczkaoryginal@gmail.com>
2022-11-06 14:58:07 +0100

    17

---
+++ JS/17. DOM.js
@@ -0,0 +1,13 @@
+Utworz formularz  rejstracji uzytkownika.
+Formularz ma sie składać z 4 inputow
+Imie,
+Nazwisko,
+email,
+hasło,
+i buttona "WYŚLIJ".
+
+Pola mają byc walidowane po wpisaniu textu (wyswietl informacje ponizej pola)
+Imie: brak pustych znakow, minimalnie 3, maxymalnie 16 liter, 1 litera ma byc duza  reszta małe, zakaz uzywania znakow specjalnych ( !@#$%^&*()) i cyfr
+Nazwisko: brak pustych znakow, minimalnie 3, maxymalnie 20 liter, 1 litera ma byc duza, reszta małe, zakaz uzywania znakow specjalnych ( !@#$%^&*()) i cyfr
+email: brak pustych znakow, minimalnie 3, maxymalnie 20 liter, dozwolone znaki specjalne ( !@#$%^&*()) i cyfry. W emailu ma się znajdowac znak @
+haslo: brak pustych znakow, minimalnie 8, maxymalnie 20 liter, powinno posiadac znaki specjalne ( !@#$%^&*()) i cyfry, oraz przynajmniej jedną dużą litere
